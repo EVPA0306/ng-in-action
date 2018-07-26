488f23685cb709841b095f8096c848cd9c19df7d
Eugene Pavlenko <C-Evgeny.Pavlenko@charter.com>
2018-07-26 16:19:40 -0600

    Before branch Angular Material (AM) version

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../shared/product.service";
 
 @Component({
   selector: 'nga-search',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
   }
