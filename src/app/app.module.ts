c4b4f51b6395c14205a0ff174274bd31cb05077f
Eugene Pavlenko <C-Evgeny.Pavlenko@charter.com>
2018-08-02 16:04:58 -0600

    Chapter 7 is over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material/'
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
+import { routes } from './app.routing';
+import { RouterModule } from '../../node_modules/@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,6 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    , MatButtonModule
+    , MatIconModule
+    , MatToolbarModule
+    , FlexLayoutModule
+    , HttpClientModule
+    , RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
